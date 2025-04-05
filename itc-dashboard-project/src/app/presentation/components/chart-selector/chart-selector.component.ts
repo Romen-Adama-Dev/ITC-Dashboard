270e0d656d4e9e9e65f4ff38762d6f279215868c
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2025-04-05 01:50:25 +0100

    style: edit modal styled

--- itc-dashboard-project/src/app/presentation/components/chart-selector/chart-selector.component.ts
+++ itc-dashboard-project/src/app/presentation/components/chart-selector/chart-selector.component.ts
@@ -16,26 +16,19 @@ interface ChartOption {
   styleUrls: ['./chart-selector.component.scss']
 })
 export class ChartSelectorVanillaComponent {
-  // Categoría seleccionada, definida desde el modal (por ejemplo, 'line-chart', 'pie-chart', 'bar-chart', etc.)
   @Input() selectedChartCategory?: string;
-  // Variante seleccionada (por ejemplo, 'line-chart', 'normalized-area-chart', etc.)
   @Input() selectedChartVariant?: string;
   @Output() selectedChartVariantChange = new EventEmitter<string>();
 
   chartOptions: ChartOption[] = [
-    // Opciones para gráficos de línea
     { label: 'Line Chart', value: 'line-chart', category: 'line-chart' },
     { label: 'Area Chart', value: 'area-chart', category: 'line-chart' },
     { label: 'Box Chart', value: 'box-chart', category: 'line-chart' },
     { label: 'Normalized Area Chart', value: 'normalized-area-chart', category: 'line-chart' },
     { label: 'Stacked Area Chart', value: 'stacked-area-chart', category: 'line-chart' },
-    
-    // Opciones para gráficos de tarta
     { label: 'Advanced Pie Chart', value: 'advanced-pie-chart', category: 'pie-chart' },
     { label: 'Pie Chart', value: 'pie-chart', category: 'pie-chart' },
     { label: 'Pie Grid Chart', value: 'pie-grid-chart', category: 'pie-chart' },
-    
-    // Opciones para gráficos de barras (horizontal y vertical)
     { label: 'Horizontal Bar Chart', value: 'horizontal-bar', category: 'bar-chart' },
     { label: 'Grouped Horizontal Bar Chart', value: 'grouped-horizontal-bar', category: 'bar-chart' },
     { label: 'Stacked Horizontal Bar Chart', value: 'stacked-horizontal-bar-chart', category: 'bar-chart' },
@@ -44,27 +37,18 @@ export class ChartSelectorVanillaComponent {
     { label: 'Grouped Vertical Bar Chart', value: 'grouped-vertical-bar', category: 'bar-chart' },
     { label: 'Stacked Vertical Bar Chart', value: 'stacked-vertical-bar-chart', category: 'bar-chart' },
     { label: 'Normalized Vertical Bar Chart', value: 'normalized-vertical-chart', category: 'bar-chart' },
-    
-    // Opciones para gauge charts
     { label: 'Gauge Chart', value: 'gauge-chart', category: 'gauge-chart' },
     { label: 'Linear Gauge Chart', value: 'linear-gauge-chart', category: 'gauge-chart' },
     { label: 'Percent Gauge Chart', value: 'percent-gauge-chart', category: 'gauge-chart' },
-    
-    // Otras opciones
     { label: 'Bubble Chart', value: 'bubble-chart', category: 'bubble-chart' },
     { label: 'Polar Chart', value: 'polar-chart', category: 'polar-chart' },
     { label: 'Graph custom', value: 'graph-custom', category: 'graph-custom' },
-    
-    // Opciones agrupadas como "Others"
     { label: 'Heat Map', value: 'heat-map', category: 'other' },
     { label: 'Number Cards', value: 'number-chart', category: 'other' },
     { label: 'Tree Map', value: 'tree-map', category: 'other' },
-    
-    // Opción Table en su propia categoría
     { label: 'Table', value: 'table', category: 'table' }
   ];
 
-  // Devuelve las opciones filtradas según la categoría seleccionada
   get filteredOptions(): ChartOption[] {
     if (!this.selectedChartCategory) {
       return [];
