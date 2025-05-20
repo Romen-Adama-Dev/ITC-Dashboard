b9fc2d634ed02692a85be68010f17352d9c7376b
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2025-05-20 12:48:31 +0100

    feat: add EditWidgetModal component for widget editing functionality

--- itc-dashboard-project/src/app/app.component.ts
+++ itc-dashboard-project/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { ThemeToggleButtonComponent } from './presentation/components/shared/buttons/theme-toggle-button.component';
-import { GridsterDashboardComponent } from "./presentation/components/gridster2/gridster2.component";
+import { GridsterDashboardComponent } from "./presentation/gridster2/gridster2.component";
 
 @Component({
   selector: 'app-root',
